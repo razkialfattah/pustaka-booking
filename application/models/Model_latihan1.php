babe228f6d9e54ecff92d30cb1a9c206268cf43b
razkialfattah <razkialfttah27@gmail.com>
2022-04-02 13:46:24 +0700

    membuat folder Model_latihan1

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -1,7 +1,7 @@
 <?php
 class Model_latihan1 extends CI_Model
 {
- //membuat variable untuk menampung nilai
+ //membuat variable untuk menampung nilaii
  public $nilai1, $nilai2, $hasil;
  //method penjumlahan
  public function jumlah($n1 = null, $n2 = null)
