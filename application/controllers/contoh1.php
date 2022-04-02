d3d400c09f50ae6e77159a7b698de4b786ad9818
razkialfattah <razkialfttah27@gmail.com>
2022-04-02 13:06:58 +0700

    membuat pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Razki Alfattah
+ Saya tingga di daerah Tangerang
+ olah raga yang saya sukai adalah
+ joging";
+ }
+}
+?>
