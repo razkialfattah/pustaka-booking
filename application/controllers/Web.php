5855e02ab599a9ccccd66adc3d2187a6d145b947
razkialfattah <razkialfttah27@gmail.com>
2022-04-03 19:51:39 +0700

    membuat folder v_about.php

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -7,10 +7,10 @@ class web extends CI_Controller{
         parent::__construct();
         $this->load->helper('url');
     }
-    public function index(){
-        $data['judul'] = "Halaman Abaut";
+    public function about(){
+        $data['judul'] = "Halaman About";
         $this->load->view('v_header',$data);
-        $this->load->view('v_index',$data);
+        $this->load->view('v_about',$data);
         $this->load->view('v_footer',$data);
     }
 }
